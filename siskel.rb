7f9ca6581f7fb6dc58d07c863e88274883dc31a3
Jonathan Rose <jonr87ee@gmail.com>
2015-06-30 15:09:07 -0400

    Hackathon

--- siskel.rb
+++ siskel.rb
@@ -1,10 +1,37 @@
 require 'httparty'
 
 class Siskel
+	attr_reader :title, :rating, :year, :plot, :concensus, :actor
+	def initialize(title, options = nil)
+    
+    if options.nil?	
+    	movie = HTTParty.get("http://www.omdbapi.com/?t=#{title}&tomatoes=true")
+    elsif !options[:year].nil?
+    	new_year = options[:year]
+    	movie = HTTParty.get("http://www.omdbapi.com/?t=#{title}&y=#{new_year}")
+    else
+    	plot = options[:plot].to_s
+    	movie = HTTParty.get("http://www.omdbapi.com/?t=#{title}&plot=#{plot}")
+    end
 
-  def initialize(title)
-    movie = HTTParty.get("http://www.omdbapi.com/?t=#{title}")
-    @title = movie["Title"]
+    if movie["Response"] == "False"
+    	@title = "Movie not found!"
+    else
+	    @title = movie["Title"]
+	    @actor = movie["Actors"].split(", ")
+	    # @actor = @actor
+	    @rating = movie["Rated"]
+	    @year = movie["Year"]
+	    @plot = movie["Plot"]
+	    tomatoes = movie["tomatoUserMeter"].to_i
+	    if tomatoes > 75 && tomatoes < 101
+	    	@concensus = "Two Thumbs Up"
+	    end
+
+	    if tomatoes < 51 && tomatoes > 25
+	    	@concensus = "Thumbs Down"
+	    end
+	end
   end
 end 
 
